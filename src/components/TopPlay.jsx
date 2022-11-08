7485d47f88d655e3bb3df0be9315b973ed7834e5
Filip Popovski <popovski_f@hotmail.com>
2022-11-08 17:23:19 +0100

    sidebar navlink changes

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,19 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper";
+import { useSelector, useDispatch } from "react-redux";
+import { FreeMode } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
+import PlayPause from "./PlayPause";
+import { playPause, setACtiveSong } from "../redux/features/playerSlice";
+import { useGetTopChartQuery } from "../redux/services/shazamCore";
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartQuery();
+  const divRef = useRef(null);
+};
 
 export default TopPlay;
