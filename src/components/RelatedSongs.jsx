8c91048e7e5b600c8925d98655231aa125048405
Filip Popovski <popovski_f@hotmail.com>
2022-11-11 18:16:08 +0100

    details header component

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import SongBar from "./SongBar";
+
 const RelatedSongs = ({
   data,
   artistId,
@@ -7,22 +9,26 @@ const RelatedSongs = ({
   handlePauseClick,
   handlePlayClick,
 }) => {
-  <div className="flex flex-col">
-    <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+  return (
+    <div className="flex flex-col">
+      <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
 
-    <div className="mt-6 w-full flex flex-col">
-      {data?.map((song, i) => (
-        <SongBar
-          key={`${song.key} - ${artistId}`}
-          song={song}
-          artistId={artistId}
-          activeSong={activeSong}
-          handlePauseClick={handlePauseClick}
-          handlePlayClick={handlePlayClick}
-        />
-      ))}
+      <div className="mt-6 w-full flex flex-col">
+        {data?.map((song, i) => (
+          <SongBar
+            key={`${artistId}-${song.key}-${i}`}
+            song={song}
+            i={i}
+            artistId={artistId}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            handlePauseClick={handlePauseClick}
+            handlePlayClick={handlePlayClick}
+          />
+        ))}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default RelatedSongs;
